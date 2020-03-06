52c4b1e49f341bcd761fc19dfd8216e9699ebf1a
tiankai <11510109@mail.sustc.edu.cn>
2020-03-06 13:35:46 -0500

    fixed a bug in Categories Table

--- 03_dumpData/10_insertCategories.py
+++ 03_dumpData/10_insertCategories.py
@@ -9,6 +9,7 @@ with open('../raw.nosync/categories.txt', 'r') as f:
     for line in f:
         u = line[:-1].split(', ')
         s = 'insert into Categories (category_id, category_name) values (' + u[0] + ', "' + u[1] + '");'
+        # s = 'UPDATE Categories SET category_name = "' + u[1] + '" WHERE category_id = ' + u[0] + ';'
         # print(s)
         cur.execute(s)
 cur.execute("commit;")
